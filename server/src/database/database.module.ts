3ede38467b4290c46981c2c98a97fa8b5a3bbda6
Cheolu.B <dev.cheolubak@gmail.com>
2022-11-27 11:34:26 +0900

    Feat: Add database module with Typeorm

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT || 3306),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
